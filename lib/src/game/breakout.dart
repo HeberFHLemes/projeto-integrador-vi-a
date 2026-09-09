bde56f299be824add339a327375618f71a504a40
HeberFHLemes <hfhlemes@gmail.com>
2026-09-09 16:17:53 -0300

    feat: adicionando parede de blocos na tela de jogo e configurando eventos de interação de início de jogo

--- lib/src/game/breakout.dart
+++ lib/src/game/breakout.dart
@@ -1,18 +1,27 @@
 import 'dart:async';
 
-import 'package:breakout/src/game/components/play_area.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import '../config/brick_color_pattern.dart';
+import '../config/brick_size.dart';
+import 'components/brick.dart';
+import 'components/play_area.dart';
 import 'constants.dart';
+import 'level_maker.dart';
 
 /// Classe responsável pelo gerenciamento do jogo e seus componentes,
 /// estendendo de FlameGame.
 class Breakout extends FlameGame
   with HasCollisionDetection, KeyboardEvents, TapCallbacks {
 
-  Breakout() : super(
+  Breakout({
+    this.brickSize = BrickSize.medium,
+    this.brickColorPattern = BrickColorPattern.classico,
+  }) : super(
     camera: CameraComponent.withFixedResolution(
       width: gameWidth,
       height: gameHeight,
@@ -22,8 +31,17 @@ class Breakout extends FlameGame
   double get width => size.x;
   double get height => size.y;
 
+  /// Tamanho dos blocos selecionado
+  BrickSize brickSize;
+
+  /// Padrão de cores selecionado para a parede de blocos
+  BrickColorPattern brickColorPattern;
+
   /// Nível atual
   int currentLevel = 1;
+
+  /// Parede de blocos que representa o nível atual
+  late List<Brick> level;
 
   @override
   FutureOr<void> onLoad() async {
@@ -42,4 +60,42 @@ class Breakout extends FlameGame
   // - Eventos de interação do usuário
   // - Usar efeitos sonoros em certas ações
   // - Gerenciar os níveis
+
+  void startGame() {
+
+    world.removeAll(world.children.query<Brick>());
+
+    level = LevelMaker.generateLevel(
+        level: currentLevel,
+        maxWidth: gameWidth,
+        brickSize: brickSize,
+        colorPattern: brickColorPattern
+    );
+
+    world.addAll(level);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    super.onTapDown(event);
+    startGame();
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+      KeyEvent event,
+      Set<LogicalKeyboardKey> keysPressed,
+      ) {
+    super.onKeyEvent(event, keysPressed);
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        // TODO: movimento p/ esquerda
+      case LogicalKeyboardKey.arrowRight:
+        // TODO: movimento p/ direita
+      case LogicalKeyboardKey.space:
+      case LogicalKeyboardKey.enter:
+        startGame();
+    }
+    return KeyEventResult.handled;
+  }
 }
