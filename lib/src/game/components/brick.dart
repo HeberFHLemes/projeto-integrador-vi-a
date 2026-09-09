011dcc45fb0f180ce86f0d42e89c0d6c0efa6501
HeberFHLemes <hfhlemes@gmail.com>
2026-09-09 15:23:55 -0300

    feat: adicionando borda aos blocos

--- lib/src/game/components/brick.dart
+++ lib/src/game/components/brick.dart
@@ -2,6 +2,7 @@ import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
+import '../../app/app_theme.dart';
 import '../breakout.dart';
 
 /// Componente que representa os blocos
@@ -30,5 +31,19 @@ class Brick extends RectangleComponent
     super.onCollisionStart(intersectionPoints, other);
 
     // TODO: lidar com as colisões
+  }
+
+  // Borda dos blocos
+  final Paint _borderPaint = Paint()
+    ..color = AppTheme.secondaryColor
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 2.4;
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+    // Adicionando borda aos blocos para poder utilizar cores claras.
+    canvas.drawRect(size.toRect(), _borderPaint);
   }
 }
