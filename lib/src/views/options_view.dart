417456e07acf3d2d53aa005f76ba6ca417ef1e25
HeberFHLemes <hfhlemes@gmail.com>
2026-09-12 12:23:30 -0300

    feat: adicionando componentes de configuração na tela de Opções

--- lib/src/views/options_view.dart
+++ lib/src/views/options_view.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import '../config/brick_color_pattern.dart';
 import '../config/brick_size.dart';
 import '../config/game_options.dart';
+import '../widgets/brick_size_button.dart';
+import '../widgets/color_pattern_dropdown.dart';
 import '../widgets/custom_app_bar.dart';
 
 /// Tela de configuração da parede de blocos por parte do usuário.
@@ -42,8 +44,63 @@ class _OptionsViewState extends State<OptionsView> {
         child: Padding(
           padding: const EdgeInsetsGeometry.all(20),
           child: Column(
+            spacing: 4,
             children: [
-              // TODO: adicionar os componentes
+              // espaço adicional entre a appbar e o "título"
+              const SizedBox(height: 32),
+              
+              // Título/header em destaque "Parede de blocos"
+              Center(
+                child: const Text(
+                  'Parede de blocos',
+                  style: TextStyle(fontSize: 20),
+                )
+              ),
+
+              // Dropdown com as opções de padrão de cores
+              Expanded(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsetsGeometry.only(top: 16),
+                    child: ColorPatternDropdown(
+                      selectedColor: _selectedColorPattern,
+                      onChanged: (pattern) {
+                        setState(() {
+                          _selectedColorPattern = pattern;
+                        });
+                      }
+                    ),
+                  ),
+                ),
+              ),
+
+              // Espaço adicional entre os componentes de configuração
+              const SizedBox(height: 20),
+
+              // Botões (SegmentedButton) para escolher os tamanhos dos blocos
+              Expanded(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(12),
+                      child: Center(
+                        child: const Text('Tamanho dos blocos'),
+                      ),
+                    ),
+
+                    BrickSizeButton(
+                      selectedSize: _selectedSize,
+                      onChanged: (size) {
+                        setState(() {
+                          _selectedSize = size;
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ),
+
+              // TODO: botão voltar
             ],
           )
         ),
